52af7b122c8df3ee16a712091f78f5c3185ba8a1
Colin <zephyr.raine@gmail.comp>
2020-09-17 16:31:52 +0200

    test

--- Assets/ShowObject.cs
+++ Assets/ShowObject.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class ShowObject : MonoBehaviour
 {
     public void ToggleProductVisibility(GameObject product)
     {
         product.SetActive(!product.activeSelf);
+    }
+
+    private void Start()
+    {
+        Invoke("Sloko", 2f);
+    }
+
+    public void Sloko()
+    {
+        FindObjectOfType<ARTrackedImageManager>().enabled = false;
     }
 }

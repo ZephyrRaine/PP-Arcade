c5cc698460413e941a640d06c5fc741b4eb337bc
Colin <zephyr.raine@gmail.comp>
2020-09-17 16:25:11 +0200

    okok

---
+++ Assets/InstanceFixedPositionRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanceFixedPositionRotation : MonoBehaviour
+{
+    public GameObject prefabToInstantiate;
+    private void Start()
+    {
+        Instantiate(prefabToInstantiate, transform.position, transform.rotation);
+    }
+}

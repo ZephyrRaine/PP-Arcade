3a6e5ed860a410a5e3415b4015bc3ce8f850ceaa
Colin <zephyr.raine@gmail.comp>
2020-09-17 16:55:56 +0200

    okok

--- Assets/InstanceFixedPositionRotation.cs
+++ Assets/InstanceFixedPositionRotation.cs
@@ -7,6 +7,11 @@ public class InstanceFixedPositionRotation : MonoBehaviour
     public GameObject prefabToInstantiate;
     private void Start()
     {
+        Invoke("OK", 0.25f);
+    }
+
+    public void OK()
+    { 
         Instantiate(prefabToInstantiate, transform.position, transform.rotation);
     }
 }
